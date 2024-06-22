e7ecfc4f42dd30669598c9f27477be0fecbbed20
Ken Nixon <nixon.kj@gmail.com>
2024-06-22 20:30:43 +0200

    Update psp_global.R

--- R/psp_global.R
+++ R/psp_global.R
@@ -69,7 +69,7 @@ psp_control <- function(radius = 0.1, init, lower, upper,
     if (parallel == TRUE && is.null(cl)) {
         #no_cores <- parallel::detectCores()
         #no_cores <- Rmpi::mpi.universe.size() - 1
-        no_cores <- 7
+        no_cores <- 3
         cl <- parallel::makeCluster(no_cores, type = "MPI")
         print(cl)
     } else if (parallel == TRUE && !is.null(cl)) {
